1bbb9de2c277564a237be48d74082d60d956daff
kim <rla5378@naver.com>
2021-08-24 11:37:34 +0900

    학생관리

---
+++ src/com/care/def/MainClass.java
@@ -0,0 +1,23 @@
+package com.care.def;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("[학생 관리]");
+			System.out.println("1.김예진 2.이나영 3.구건회 4.김하용\n>>>>");;
+			num = input.nextInt();
+			switch(num) {
+			case 1:break;
+			case 2:break;
+			case 3:break;
+			case 4:break;
+			}
+		}
+	}
+
+}
