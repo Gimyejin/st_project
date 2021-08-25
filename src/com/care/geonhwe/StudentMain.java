97fd061d5e2bcfd4c8c6a407513caec22ef06c07
Koo Geonhwe <mashrjs@naver.com>
2021-08-25 22:58:48 +0900

    HashMap기능추가

--- src/com/care/geonhwe/StudentMain.java
+++ src/com/care/geonhwe/StudentMain.java
@@ -1,14 +1,28 @@
 package com.care.geonhwe;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
 import java.util.Scanner;
+
+class hash {
+	int age = 0;
+	String name, gender, addr;
+	public hash(int age, String name, String gender) {
+		this.age = age;
+		this.name = name;
+		this.gender = gender;
+		this.addr = addr;
+	}
+	String set() {
+		return name+"|"+addr;
+	}
+}
 
 public class StudentMain {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Student> stu = new ArrayList<Student>();
-		HashMap<Integer,String> map = new HashMap<Integer,String>();
+		HashMap<hash,String> map = new HashMap<hash,String>();
 		String name, gender, addr;
 		int age;
 		
@@ -43,10 +57,28 @@ public class StudentMain {
 					System.out.println("주소 : "+s.getAddr());
 					System.out.println("---------------------");
 					break; }
+			case 3:
+				for(int j=1; j<6; j++) {
+					System.out.println(j+"번째 학생이름|성별|나이|주소");
+					name = sc.next();
+					gender = sc.next();
+					age = sc.nextInt();
+					addr = sc.next();
+					
+				map.put(new hash(age, gender, addr), name);
+				}
+				System.out.println("찾고 싶은 학생의 이름을 입력");
+				Scanner sc2 = new Scanner(System.in);
+				String getName = sc2.next();
+				String has = map.get(getName);
+				
+				System.out.println(getName + "님의 정보");
+			}
+				
 			}
 		}
 	}
-}
+
 
 
 
